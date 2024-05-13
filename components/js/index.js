93c28b8426919dafb2c42efde0ca3c3960b0cb1c
Fernando <fe.bonifacio@hotmail.com>
2024-05-13 16:25:55 -0300

    exemplos

---
+++ components/js/index.js
@@ -0,0 +1,16 @@
+let popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="popover"]'),
+)
+
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
+
+let tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]'),
+)
+
+let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
